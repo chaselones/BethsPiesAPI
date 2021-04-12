f205a5ec5433cc43893c549316cdf4dbb992a560
Chase Lones <chaselones@gmail.com>
2021-04-11 19:31:04 -0700

    added search route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Bring in Express server and create application
 const express = require("express");
-const pieRepo = require("./repos/pieReo");
+const pieRepo = require("./repos/pieRepo");
 const app = express();
 
 //Set port
@@ -22,6 +22,49 @@ router.get("/", function (req, res, next) {
     next(err);
   })
   
+});
+
+router.get("/search", function(req,res,next){
+  let searchObject = {
+    "id": req.query.id,
+    "name": req.query.name
+  };
+
+  pieRepo.search(searchObject, function(data){
+    res.status(200).json({
+      "status": 200,
+      "statusText": "OK",
+      "message": "Pies are here!",
+      "data": data
+    });
+  }, function (err){
+    next(err);
+  });
+});
+
+router.get('/:id', function(req,res,next){
+  pieRepo.getById(req.params.id, function(data){
+    if(data){
+      res.status(200).json({
+        "status": 200,
+        "statusText": "OK",
+        "message": "Pie retrieved",
+        "data": data
+      });
+    }else{
+      res.status(404).json({
+        "status": 404,
+        "statusText": "Not Found",
+        "message": "The Pie " + req.params.id + " could not be found.",
+        "error": {
+          "code": "Not found",
+          "message": "The pie " + req.params.id + " could not be found."
+        }
+      });
+    }
+  }, function(err){
+    next(err);
+  });
 });
 
 //Configure router so all routes are prefixed with /api/v1
